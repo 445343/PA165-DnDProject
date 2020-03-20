2a2ba04f5cea9dc75376cf3cfd2d8cccaca354e5
Alena Bednaříková <xbednar8@fi.muni.cz>
2020-03-21 00:22:16 +0100

    Role entity and DAO created

--- dnd-persistence/src/main/java/cz/fi/muni/PA165/persistence/model/Role.java
+++ dnd-persistence/src/main/java/cz/fi/muni/PA165/persistence/model/Role.java
@@ -1,4 +1,66 @@
 package cz.fi.muni.PA165.persistence.model;
 
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity(name = "Role")
+@Table(name = "role")
 public class Role {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Role)) return false;
+        Role role = (Role) o;
+        return getId().equals(role.getId()) &&
+                getName().equals(role.getName()) &&
+                Objects.equals(getDescription(), role.getDescription());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getDescription());
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
