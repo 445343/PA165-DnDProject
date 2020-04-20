87eb1655f82f16319478aa7d210b49067382ba2f
Michal Čaniga <michalcaniga559@gmail.com>
2020-04-20 16:06:53 +0200

    Additional service methods

--- service/src/main/java/cz/fi/muni/PA165/service/HeroServiceImpl.java
+++ service/src/main/java/cz/fi/muni/PA165/service/HeroServiceImpl.java
@@ -1,8 +1,12 @@
 package cz.fi.muni.PA165.service;
 
 import cz.fi.muni.PA165.persistence.dao.HeroDao;
+import cz.fi.muni.PA165.persistence.dao.RoleDao;
+import cz.fi.muni.PA165.persistence.dao.TroopDao;
 import cz.fi.muni.PA165.persistence.model.Hero;
 import cz.fi.muni.PA165.api.exceptions.DnDServiceException;
+import cz.fi.muni.PA165.persistence.model.Role;
+import cz.fi.muni.PA165.persistence.model.Troop;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,10 +21,14 @@ import java.util.List;
 public class HeroServiceImpl implements HeroService {
 
     private HeroDao heroDao;
+    private RoleDao roleDao;
+    private TroopDao troopDao;
 
     @Autowired
-    public HeroServiceImpl(HeroDao heroDao) {
+    public HeroServiceImpl(HeroDao heroDao, RoleDao roleDao, TroopDao troopDao) {
         this.heroDao = heroDao;
+        this.roleDao = roleDao;
+        this.troopDao = troopDao;
     }
 
     @Override
@@ -50,5 +58,47 @@ public class HeroServiceImpl implements HeroService {
     @Override
     public List<Hero> findAllHeroes() {
         return heroDao.findAll();
+    }
+
+    @Override
+    public void addRole(Long heroId, Long roleId) {
+        Hero hero = findById(heroId);
+        Role role = findRoleById(roleId);
+        hero.addRole(role);
+    }
+
+    @Override
+    public void removeRole(Long heroId, Long roleId) {
+        Hero hero = findById(heroId);
+        Role role = findRoleById(roleId);
+        hero.removeRole(role);
+    }
+
+    @Override
+    public void joinTroop(Long heroId, Long troopId) {
+        Hero hero = findById(heroId);
+        Troop troop = findTroopById(troopId);
+        hero.joinTroop(troop);
+    }
+
+    @Override
+    public void leaveTroop(Long heroId, Long troopId) {
+        Hero hero = findById(heroId);
+        Troop troop = findTroopById(troopId);
+        hero.leaveTroop(troop);
+    }
+
+    private Role findRoleById(Long id) {
+        Role role = roleDao.findById(id);
+        if (role == null)
+            throw new DnDServiceException("Role with " + id + "not found");
+        return role;
+    }
+
+    private Troop findTroopById(Long id) {
+        Troop troop = troopDao.findById(id);
+        if (troop == null)
+            throw new DnDServiceException("Troop with " + id + "not found");
+        return troop;
     }
 }
