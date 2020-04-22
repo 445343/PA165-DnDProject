d2647ba81a2d02667d71c3f81d157437594b3b2d
Alena Bednaříková <xbednar8@fi.muni.cz>
2020-04-22 21:37:19 +0200

    created and implemented Troop DTO

---
+++ api/src/main/java/cz/fi/muni/PA165/api/dto/troop/TroopUpdateDTO.java
@@ -0,0 +1,81 @@
+package cz.fi.muni.PA165.api.dto.troop;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.Objects;
+
+/**
+ * Encapsulates information for troop update
+ *
+ * @author Alena Bednarikova
+ */
+
+public class TroopUpdateDTO {
+    @NotNull(message = "Id can not be null")
+    private Long id;
+    @NotEmpty(message = "Name can not be empty")
+    private String name;
+    @Min(0)
+    private int gold;
+
+    private String mission;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getGold() {
+        return gold;
+    }
+
+    public void setGold(int gold) {
+        this.gold = gold;
+    }
+
+    public String getMission() {
+        return mission;
+    }
+
+    public void setMission(String mission) {
+        this.mission = mission;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof TroopUpdateDTO)) return false;
+        TroopUpdateDTO that = (TroopUpdateDTO) o;
+        return getGold() == that.getGold() &&
+                Objects.equals(getId(), that.getId()) &&
+                Objects.equals(getName(), that.getName()) &&
+                Objects.equals(getMission(), that.getMission());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getGold(), getMission());
+    }
+
+    @Override
+    public String toString() {
+        return "TroopUpdateDTO{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", gold=" + gold +
+                ", mission='" + mission + '\'' +
+                '}';
+    }
+}
