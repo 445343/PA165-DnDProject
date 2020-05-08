afe4d45b8facb606ed80034e3b4af185ef16f962
Jadus <445343@mail.muni.cz>
2020-05-08 21:17:40 +0200

    /pa165 is clickable

--- rest/src/main/java/cz/fi/muni/PA165/rest/config/WebContextConfig.java
+++ rest/src/main/java/cz/fi/muni/PA165/rest/config/WebContextConfig.java
@@ -7,14 +7,8 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.format.FormatterRegistry;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.validation.MessageCodesResolver;
-import org.springframework.validation.Validator;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
-import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.*;
 
 import java.util.List;
