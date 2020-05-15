4d12b8f3ee66db6a4be6a9d640fde8d9f24157a8
Jan Válka <hondak57@seznam.cz>
2020-05-15 20:14:19 +0200

    adding roles to heroes now works

--- web-app/src/app/components/heroes/heroes.component.ts
+++ web-app/src/app/components/heroes/heroes.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {HeroService} from "../../services/hero/hero.service";
+import {RoleService} from "../../services/role/role.service";
 import {Observable} from "rxjs";
 import {HeroDTO} from "../../dto/hero/HeroDTO";
+import {RoleDTO} from "../../dto/role/RoleDTO";
 
 @Component({
   selector: 'app-heroes',
@@ -10,23 +12,51 @@ import {HeroDTO} from "../../dto/hero/HeroDTO";
 })
 export class HeroesComponent implements OnInit {
   heroes: Observable<HeroDTO>;
+  roles: Observable<RoleDTO>;
   tmp;
+  showModal = false;
+  clickedHeroId;
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService,
+              private roleService: RoleService) { }
 
   ngOnInit(): void {
     this.loadHeroes();
+    this.loadRoles();
   }
 
   loadHeroes(){
     this.heroService.getAllHeroes().subscribe(response =>{
       this.tmp = response;
       this.heroes = this.tmp.content;
+      console.log(this.heroes);
+    });
+  }
+
+  loadRoles(){
+    this.roleService.getAllRoles().subscribe(response =>{
+      this.tmp = response;
+      this.roles = this.tmp.content;
     });
   }
 
   deleteHero(id){
     this.heroService.deleteHero(id).subscribe(response =>{
+      this.loadHeroes();
+    });
+  }
+
+  addRoleModal(heroId){
+    this.clickedHeroId = heroId;
+    this.showModal = true;
+
+  }
+  closeRoleModal(){
+    this.clickedHeroId = 0;
+    this.showModal = false;
+  }
+  addRole(roleId){
+    this.heroService.addRoleToHero(this.clickedHeroId, roleId).subscribe(response =>{
       this.loadHeroes();
     });
   }
